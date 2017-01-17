8ef4d4618263bdd5361a5e2dca31b767377da4e9
Unknown <lcampara96@gmail.com>
2017-01-17 18:58:05 +0100

    update con array

--- TapChallenge/ViewController.m
+++ TapChallenge/ViewController.m
@@ -11,6 +11,8 @@
 #define GameTimer 1
 #define GameTime 5
 #define FirstAppLaunch @"FirsrAppLaunch"
+#define Defaults [NSUserDefaults standardUserDefaults]
+#define Results @"UserScore"
 
 @interface ViewController () {
     int _tapsCount;
@@ -33,13 +35,18 @@
 -(void)viewDidAppear:(BOOL)animated {
     
     if ([self firstAppLaunch] == false) {
-        [[NSUserDefaults standardUserDefaults]setBool:true forKey:FirstAppLaunch];
+        [Defaults setBool:true forKey:FirstAppLaunch];
         [[NSUserDefaults standardUserDefaults]synchronize];
     } else {
-        int ultimoRisultato = [self getRisultato];
+        if ([self risultati].count > 0) {
+            NSNumber *value = [self risultati].lastObject;
+            [self mostraUltimoRisultato: value.intValue];
+        }
+        
+        /*int ultimoRisultato = [self getRisultato];
         if (ultimoRisultato > 0) {
             [self mostraUltimoRisultato: ultimoRisultato];
-        }
+        }*/
     }
 }
 
@@ -102,8 +109,8 @@
 #pragma mark - UI
 
 -(void)mostraUltimoRisultato: (int)risultato {
-    NSString *message = [NSString stringWithFormat:@"Ultimo risultato: %i", risultato];
-    UIAlertController *alertViewController = [UIAlertController alertControllerWithTitle:@"Ultimo risultato" message:message preferredStyle:UIAlertControllerStyleAlert];
+    NSString *message = [NSString stringWithFormat:@"Il tuo miglior risultato: %i", risultato];
+    UIAlertController *alertViewController = [UIAlertController alertControllerWithTitle:@"Migliore risultato" message:message preferredStyle:UIAlertControllerStyleAlert];
     
     UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
         NSLog(@"ACTION PREMUTA");
@@ -116,20 +123,73 @@
 #pragma mark - Persistenza
 
 -(void)salvaRisultato {
+    
+    NSMutableArray *array = [[Defaults objectForKey:Results] mutableCopy];
+    
+    if (array == nil) {
+        //old
+        //array = [[NSMutableArray alloc]init].mutableCopy;
+        array = @[].mutableCopy;
+    }
+    
+    // converto l'intero in NSNumber ??
+    NSNumber *number = [NSNumber numberWithInt:_tapsCount];
+    [array addObject: number];
+    
+    NSLog(@"array -> %@", array);
+    
+    NSArray *arrayToBeSaved = [array sortedArrayUsingComparator:^NSComparisonResult(NSNumber *obj1, NSNumber *obj2) {
+        int value1 = obj1.intValue;
+        int value2 = obj2.intValue;
+        
+        if (value1 == value2) {
+            return NSOrderedSame;
+        }
+        
+        if (value1 < value2) {
+            return NSOrderedAscending;
+        }
+        
+        return NSOrderedDescending;
+    }].mutableCopy;
+    
+    [Defaults setObject:arrayToBeSaved forKey:Results];
+    [Defaults synchronize];
+    
+    
+    /*
+    //***vecchia versione per salvare un intero singolo***
+     
+     
     // salvare un dato nelle preferenze dell'app (Shared Preferences in android)
     [[NSUserDefaults standardUserDefaults] setInteger:_tapsCount forKey:@"TapsCount"];
     
     //per sincronizzare subito i dati
     [[NSUserDefaults standardUserDefaults] synchronize];
+     */
 }
 
--(int)getRisultato {
+-(NSArray *)risultati {
+    NSArray *array = [Defaults objectForKey:Results];
+    
+    if (array == nil) {
+        
+        //inizializzo l'array statico
+        array = @[].mutableCopy;
+    }
+    
+    NSLog(@"VALORE  %@", array);
+    
+    return array;
+}
+
+/*-(int)getRisultato {
     // riprendo il valore
     int value = [[NSUserDefaults standardUserDefaults] integerForKey:@"TapsCount"];
     NSLog(@"RISULTATO %i", value);
     
     return value;
-}
+}*/
 
 -(bool)firstAppLaunch {
     return [[NSUserDefaults standardUserDefaults] boolForKey:FirstAppLaunch];
